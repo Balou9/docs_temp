0c286e31a892206c52f09eeff748470bf3aae61b
Balou9 <emmanuel.owosekun@gmail.com>
2018-03-17 07:41:11 +0100

    see documentation

---
+++ index.js
@@ -0,0 +1,12 @@
+/** This is a description of the foo function. */
+function foo() {
+}
+
+
+/**A table object */
+class Table {
+  constructor(width, height) {
+    this.width = width
+    this.height = height
+  }
+}
---
+++ index2.js
@@ -0,0 +1,5 @@
+/** Basic Documentation*/
+
+/** Does this work?*/
+
+/** It does!*/
